c248ebf692de27ddab1f622b2958d3fbd96c1e0e
HobMartin <ivanderda1999@gmail.com>
2021-12-12 17:36:47 +0200

    init

---
+++ app.py
@@ -0,0 +1,54 @@
+import os
+from flask import Flask, request, render_template
+import cv2
+from tensorflow import keras
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+import numpy as np
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+model = keras.models.load_model("BrainTumor10EpochsCaterogical.h5")
+print("Model loaded")
+
+
+def get_class_name(classNo):
+    if classNo == 0:
+        return "No Brain Tumor"
+    elif classNo == 1:
+        return "Yes Brain Tumor"
+
+
+def get_result(img):
+    image = cv2.imread(img)
+    image = Image.fromarray(image, "RGB")
+    image = image.resize((64, 64))
+    image = np.array(image)
+    input_image = np.expand_dims(image, axis=0)
+    predict = model.predict(input_image)
+    result = np.argmax(predict, axis=1)
+    return result
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        f = request.files["file"]
+
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(basepath, "uploads", secure_filename(f.filename))
+        f.save(file_path)
+        value = get_result(file_path)
+        result = get_class_name(value)
+        return result
+    return None
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ train.py
@@ -0,0 +1,94 @@
+import cv2
+import os
+import tensorflow as tf
+from tensorflow import keras
+from PIL import Image
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+# from keras.utils import normalize
+# from keras.models import Sequential
+# from keras.layers import Conv2D, MaxPolling2D
+# from keras.layers import Activation, Dropout, Flatten, Dense
+
+image_dir = "dataset/"
+
+no_tumor = os.listdir(image_dir + "no/")
+yes_tumor = os.listdir(image_dir + "yes/")
+
+dataset = []
+label = []
+
+INPUT_SIZE = 64
+
+#
+for i, image_name in enumerate(no_tumor):
+    if image_name.split(".")[1] == "jpg":
+        image = cv2.imread(image_dir + "no/" + image_name)
+        image = Image.fromarray(image, "RGB")
+        image = image.resize((INPUT_SIZE, INPUT_SIZE))
+        dataset.append(np.array(image))
+        label.append(0)
+
+#
+for i, image_name in enumerate(yes_tumor):
+    if image_name.split(".")[1] == "jpg":
+        image = cv2.imread(image_dir + "yes/" + image_name)
+        image = Image.fromarray(image, "RGB")
+        image = image.resize((INPUT_SIZE, INPUT_SIZE))
+        dataset.append(np.array(image))
+        label.append(1)
+
+dataset = np.array(dataset)
+label = np.array(label)
+
+x_train, x_test, y_train, y_test = train_test_split(
+    dataset, label, test_size=0.2, random_state=0
+)
+
+x_train = keras.utils.normalize(x_train, axis=1)
+x_test = keras.utils.normalize(x_test, axis=1)
+
+y_train = keras.utils.to_categorical(y_train, num_classes=2)
+y_test = keras.utils.to_categorical(y_test, num_classes=2)
+
+# Model Building
+
+model = keras.models.Sequential()
+
+model.add(keras.layers.Conv2D(32, (3, 3), input_shape=(INPUT_SIZE, INPUT_SIZE, 3)))
+model.add(keras.layers.Activation("relu"))
+model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+
+
+model.add(keras.layers.Conv2D(32, (3, 3), kernel_initializer="he_uniform"))
+model.add(keras.layers.Activation("relu"))
+model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+
+
+model.add(keras.layers.Conv2D(64, (3, 3), kernel_initializer="he_uniform"))
+model.add(keras.layers.Activation("relu"))
+model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+
+
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(64))
+model.add(keras.layers.Activation("relu"))
+model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dense(2))
+model.add(keras.layers.Activation("softmax"))
+
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+model.fit(
+    x_train,
+    y_train,
+    batch_size=16,
+    verbose=True,
+    epochs=30,
+    validation_data=(x_test, y_test),
+    shuffle=False,
+)
+
+model.save("BrainTumor10EpochsCaterogical.h5")
